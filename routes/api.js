41a86bb602bfa3466405febb63911f5339d46e30
nakkamarra <nickbrandt2012@aol.com>
2018-06-13 13:25:40 -0400

    Add express application to repo

---
+++ routes/api.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var fileSystem = require('fs');
+
+/* GET Request */
+router.get('/', function(req, res, next) {
+    res.json({
+      title: "Default JSON API",
+      description: {
+	local: "Testing",
+	extended: "Testing"
+      },
+      lookupValues: ["123", "456", "789"]
+    });
+});
+
+/* POST Request */
+//TODO: WRITE SOME POST REQUEST CODE
+
+module.exports = router;
