bf40bab73a32cbc73afa8fc82de71c7cd6973a59
nakkamarra <nickbrandt2012@aol.com>
2018-06-15 10:27:04 -0400

    Remove old webhook middleware

--- hook.js
+++ hook.js
@@ -1,20 +1,29 @@
-var gith = require('gith').create( 3500 );
-// Import execFile, to run our bash script
-var execFile = require('child_process').execFile;
+var http = require('http');
+var fs = require('fs');
+const exec = require('child_process').exec;
 
-gith({
-    repo: 'nakkamarra/home'
-}).on( 'all', function( payload ) {
-    if( payload.branch === 'master' )
-    {
-            // Exec a shell script
-            execFile('/opt/scripts/hook-redeploy.sh', function(error, stdout, stderr) {
-                    // Log success in some manner
-		    if( error ) {
-		      console.log( 'Hook script failed' + error );
-		    } else {
-                      console.log( 'Hook exec complete' );
-		    }
-            });
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  if (validateRequest(req)) {
+    res.end(JSON.stringify({'success': 'true'}));
+  } else {
+    res.end(JSON.stringify({'success': 'false'}));
+  }
+}).listen(3500);
+
+function validateRequest(request) {
+  let headers = request.headers;
+  console.log(headers);
+  if (headers['x-github-event']) {
+    return true;
+  }
+  return false;
+}
+
+function executeRedeploy() {
+  exec('redeploy.sh', function(error, stdout, stderr) {
+    if (error) {
+      console.log(error.code);
     }
-});
+  });
+}
