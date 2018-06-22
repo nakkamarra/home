fbbe4b0acd7f6553f4b452592651eaff6cf8afa4
Nick Brandt <nickbrandt2012@aol.com>
2018-06-22 14:18:30 -0400

    Update the api route

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var fileSystem = require('fs');
 
 /* GET Request */
 router.get('/', function(req, res, next) {
     res.json({
       title: "Default JSON API",
       description: {
-	local: "Testing",
-	extended: "Testing"
+	    local: "Testing",
+	    extended: "Testing"
       },
       lookupValues: ["123", "456", "789"]
     });
