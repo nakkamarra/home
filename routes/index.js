9907b74b834e67300ec67f1abaff910130e73db7
Nick Brandt <nickbrandt2012@aol.com>
2018-06-29 21:05:50 -0400

    Linking to my social sites

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const config = require('../config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nakkamarra' });
+  res.render('index', { title: 'Nakkamarra', urls: config.social_urls } );
 });
 
 module.exports = router;
